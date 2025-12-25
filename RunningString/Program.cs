075a09053dc0a7384317d6189b2a62ae30118ea7
Alexander <pchp250g8@tutamail.com>
2025-12-25 11:47:33 +0300

    Добавьте файлы проекта.

---
+++ RunningString/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.CursorVisible = false;
+var strSpaces = new String(' ', 80);
+var strLine = "Hello World!!! Running String!!! Press Any Key To Exit!!!";
+strLine = strSpaces + strLine + strSpaces;
+var nCounter = 0;
+var nStrLen = strLine.Length - 80;
+do
+{
+    strSpaces = strLine.Substring(nCounter, 80);
+    Console.SetCursorPosition(10, 10);
+    Console.Write(strSpaces);
+    Thread.Sleep(140);
+    nCounter++;
+    if(nCounter >= nStrLen) nCounter = 0;
+} while (!Console.KeyAvailable);
